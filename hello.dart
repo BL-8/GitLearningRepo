8cc62d39695b8c78505a4011f892a907eb3f73a1
BL-8 <banwari.hawkscode@gmail.com>
2020-10-12 16:16:53 +0530

    after all Changes in Dev

--- hello.dart
+++ hello.dart
@@ -1,10 +1,6 @@
 class A {
   void displayData() {
-    print("Hello");
-    print("In dev Branch");
-    print("Another Changes on dev Branch");
-    print("Master Changes");
-    print("DEVDEV");
+   print("Only Dev Branch");
   }
 }
 
